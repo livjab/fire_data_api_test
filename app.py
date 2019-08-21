1e0d4365927cb5a9a7222635c819907721b7167e
Rice_from_data <nedderlander@gmail.com>
2019-08-21 16:24:37 -0700

    added background pulling data

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ from json import dumps
 import pandas as pd
 import numpy as np
 from math import radians, cos, sin, asin, sqrt
-from apscheduler.schedulers.blocking import BlockingScheduler
 from pandas.util.testing import assert_frame_equal
+
+# Different schedulers for querying data
+from apscheduler.schedulers.background import BackgroundScheduler
+# import schedule, time
+
 
 # data source
 # https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms
@@ -42,7 +46,7 @@ def haversine(lon1, lat1, lon2, lat2):
         r = 3956 #radius of earth in miles mean of  poles and equator radius
         return c * r
 
-def check_fires(user_coords, perimiter, fire_cords):
+def check_fires(user_coords, perimiter, fire_coords):
     """
     checks a single user long/lat tuple against an array of long/lat fire coordinates
     and returns a dict of fires within the perimiter and a binary Alert: yes/no
@@ -53,17 +57,17 @@ def check_fires(user_coords, perimiter, fire_cords):
         'Fires' : []
     }
     
-    #iterate through the fire cord pairs
-    for cord in fire_cords:
+    #iterate through the fire coord pairs
+    for coord in fire_coords:
         
         #compare user_coords with individual fires
-        distance = haversine(user_coords[0], user_coords[1], cord[0], cord[1])
+        distance = haversine(user_coords[0], user_coords[1], coord[0], coord[1])
 
         # if this fire is on or within the user perimiter we set the alert flag and save the fire data
         if distance <= perimiter:
             results['Alert'] = True
             
-            fire_location_tuple = ((cord[0], cord[1]), distance)
+            fire_location_tuple = ((coord[0], coord[1]), distance)
             #the fire location tuples will have the form
 
             # list of ([long, lat], distance_to_user)
@@ -82,17 +86,17 @@ class CheckFires(Resource):
     def post(self):
         values = request.get_json()
 
-        user_cords = values['user_cords'] #I want to get a json like:
-        #{'user_cords' : (long, lat)} 
+        user_coords = values['user_coords'] #I want to get a json like:
+        #{'user_coords' : (long, lat)} 
         try:
             perimiter = values['distance']
         except:
             perimiter = 50
 
         #get's all high confidence fire coords from our df
-        fire_cords = df.loc[df['confidence'] > 90][['longitude', 'latitude']].values
+        fire_coords = df.loc[df['confidence'] > 90][['longitude', 'latitude']].values
 
-        return jsonify(check_fires(user_cords, perimiter, fire_cords))
+        return jsonify(check_fires(user_coords, perimiter, fire_coords))
 
 
 # connects resources to api endpoint
@@ -111,38 +115,46 @@ def pull_modus(url = modis_url):
 #defines our first df from memory
 df = pd.read_csv('modus_df', sep=',')
     
-def check_new_df(df=df):
+def check_new_df(df):
     """
     Pulls a new df from modus and compares it to the live df
     """
     try:
         new_df = pull_modus()
-
+#todo document
         if assert_frame_equal(df, new_df):
             pass
+
         else:
             df = new_df.copy()
 
         return df
         
     except:
-        return 'Modus URL not reachable'
+        pass # 'Modus URL not reachable'
 
 
-# TODO this scheduler is not working so this is a beta build, data is not live
-# but it is callable
 
-# # pulls a new df every hour
-# scheduler = BlockingScheduler()
-# scheduler.add_job(check_new_df, 'interval', hours=1)
-# scheduler.start()
+# pulls a new df every hour
+def check_modus_data():
+    df = check_new_df(df)
 
-# check our df status
+scheduler = BackgroundScheduler()
+scheduler.add_job(check_modus_data, 'interval', hours=1)
+scheduler.start()
+
+# check our df size
 @app.route('/data/size', methods=['GET'])
 def df_size():
     size = df.shape
     return jsonify({'df_size' : size}), 201
 
+# check our df head
+@app.route('/data/head', methods=['GET'])
+def df_head():
+    head = df.head().to_json()
+    return jsonify({'df_head' : head}), 201
+
 # Start process
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run()
